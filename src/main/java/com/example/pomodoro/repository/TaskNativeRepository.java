0dba4e408ddc3ef5dae2dfdb93d5e359c0c20eb6
bongcoy <mpascaldewantara@gmail.com>
2025-01-19 11:56:40 +0700

    update: add Modifying and Transactional dependencies. This required because the INSERT, UPDATE, and DELETE is affecting the database

--- src/main/java/com/example/pomodoro/repository/TaskNativeRepository.java
+++ src/main/java/com/example/pomodoro/repository/TaskNativeRepository.java
@@ -1,27 +1,38 @@
 package com.example.pomodoro.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.pomodoro.model.TaskModel;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface TaskNativeRepository extends JpaRepository<TaskModel, Long> {
-    @Query(value = "INSERT INTO m_task (title, is_complete) VALUES (?1, ?2)", nativeQuery = true)
-    TaskModel create(String title, boolean isComplete);
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO m_task (title, is_completed, created_on) VALUES (?1, ?2, ?3)", nativeQuery = true)
+    void create(String title, boolean isCompleted, LocalDateTime createdOn);
 
     @Query(value = "SELECT * FROM m_task", nativeQuery = true)
     List<TaskModel> findAll();
 
     @Query(value = "SELECT * FROM m_task WHERE id = ?1", nativeQuery = true)
-    TaskModel findByIdNative(Long id);
+    Optional<TaskModel> findByIdNative(Long id);
 
-    @Query(value = "UPDATE m_task SET title = ?2, is_complete = ?3 WHERE id = ?1", nativeQuery = true)
-    TaskModel update(Long id, String title, boolean isComplete);
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE m_task SET title = ?2, is_completed = ?3 , modified_on = ?4 WHERE id = ?1", nativeQuery = true)
+    void update(Long id, String title, boolean isCompleted, LocalDateTime modifiedOn);
 
+    @Modifying
+    @Transactional
     @Query(value = "DELETE FROM m_task WHERE id = ?1", nativeQuery = true)
     void delete(Long id);
 }
